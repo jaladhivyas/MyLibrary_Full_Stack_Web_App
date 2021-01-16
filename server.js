95756af5aebfba9f84e888ac541194a3569b777e
jaladhivyas <vyasjaladhi@gmail.com>
2021-01-16 12:44:54 +0530

    Added general Styles

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const mongoose = require('mongoose');
  app.use(methodOverride('_method'));
  app.use('/authors',authorRouter);
  app.use('/books',bookRouter);
- const PORT = process.env.PORT || 3000;   
+ const PORT = process.env.PORT || 5000;   
 mongoose.connect('mongodb+srv://vyasjaladhi:1Pradyuman1@cluster0.ofae0.mongodb.net/<dbname>?retryWrites=true&w=majority',{useNewUrlParser : true, useUnifiedTopology: true })
 .then(()=> app.listen(PORT,() => console.log(`Server running on port:${PORT}`)))
 .catch((error) => console.log(error.message));
